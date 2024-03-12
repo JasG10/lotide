446bcf16b59f136b20545b8fc4b71a7db5473632
Jasjot Gill <jasjot97@gmail.com>
2024-03-12 06:54:18 +0000

    made changes to middletest and tailtest due to feedback

--- test/middleTest.js
+++ test/middleTest.js
@@ -4,22 +4,22 @@ const assertArraysEqual = require('chai').assertArraysEqual;
 
 describe('middle function', () => {
     it('should return an empty array for an input array with one element', () => {
-      assertArraysEqual(middle([1]), []);
+      assert.deepEqual(middle([1]), []);
     });
   
     it('should return an empty array for an input array with two elements', () => {
-      assertArraysEqual(middle([1, 2]), []);
+      assert.deepEqual(middle([1, 2]), []);
     });
   
     it('should return an array with the middle element for an odd-length input array', () => {
-      assertArraysEqual(middle([1, 2, 3]), [2]);
+      assert.deepEqual(middle([1, 2, 3]), [2]);
     });
   
     it('should return an array with the two middle elements for an even-length input array', () => {
-      assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+      assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
     });
   
     it('should return an array with the two middle elements for an input array with more than four elements', () => {
-      assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+      assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
     });
   });
--- test/tailTest.js
+++ test/tailTest.js
@@ -5,13 +5,13 @@ describe('#tail', () => {
     it('should not modify the original array', () => {
       const words = ["Yo Yo", "Lighthouse", "Labs"];
       tail(words);
-      assert.Strictequal(words.length, 3);
+      assert.strictEqual(words.length, 3);
     });
   
     it('should return an array with one less element than the original array', () => {
       const words = ["Yo Yo", "Lighthouse", "Labs"];
       const result = tail(words);
-      assert.Strictequal(result.length, words.length - 1);
+      assert.strictEqual(result.length, words.length - 1);
     });
   
     it('should return an empty array when called on an array with one element', () => {
