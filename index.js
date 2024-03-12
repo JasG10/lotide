6971935eae49ec8af0bba58aa2399389000bcf37
Jasjot Gill <jasjot97@gmail.com>
2024-03-12 07:47:45 +0000

    added more fucntions to index file

--- index.js
+++ index.js
@@ -5,23 +5,34 @@ const middle = require('./middle');
 const assertArraysEqual = require ('./assertArraysEqual');
 const assertEqual = require ('./assertEqual');
 const assertObjectsEqual = require ('./assertObjectsEqual');
-const countLetters = require ('./countLetters');
 const countOnly = require ('./countOnly');
 const countLetters = require ('./countLetters');
 const eqArrays = require ('./eqArrays');
 const eqObjects = require ('./eqObjects');
+const findKey = require ('./findKey');
+const findKeyByValue = require ('./findKeyByValue');
+const flatten = require ('./flatten');
+const letterPositions = require ('./letterPositions');
+const takeUntil = require ('./takeUntil');
+const without = require ('./without');
 
 module.exports = {
   head:   head,
   tail:   tail,
   middle: middle,
-  assertArrayEquals: assertArrayEquals,
+  assertArrayEqual: assertArrayEqual,
   assertEqual: assertEqual,
   assertObjectsEqual: assertObjectsEqual,
   countLetters: countLetters,
   countOnly: countOnly,
   eqArrays: eqArrays,
   eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  takeUntil: takeUntil,
+  without: without,
 
 };
 
