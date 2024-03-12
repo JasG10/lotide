b3c503d961bac6a9a5d1fe1cede78ef2598d845c
Jasjot Gill <jasjot97@gmail.com>
2024-03-12 06:10:55 +0000

    added assertarrayequaltest file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,17 +6,9 @@ const assertArraysEqual = function(actual, expected) {
     }
 };
 
-const eqArrays = function (ar1, ar2) {
-    if (ar1.length !== ar2.length){
-        return false;
-    }
-    for (let i = 0; i < ar1.length; i++){
-        if (ar1[i] !== ar2[i]){
-            return false;
-        }
-    }
-    return true;
-}
+
+
+module.exports = assertArraysEqual;
 
 
 
