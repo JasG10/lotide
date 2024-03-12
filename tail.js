9ed1dcac96a9958a838315a72844bae14058f9b3
Jasjot Gill <jasjot97@gmail.com>
2024-03-12 05:52:36 +0000

    tailtest updated

--- tail.js
+++ tail.js
@@ -11,9 +11,5 @@ const tail = function(arr) {
   return arr.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(words).length, words.length - 1);
-assertEqual(tail(["test"]).length, 0);
-assertEqual(tail([]).length, 0);
+module.exports = tail;
+
