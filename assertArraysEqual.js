60c3691f8c1f9af5afd8cb16ddb8576195102344
Jasjot Gill <jasjot97@gmail.com>
2024-03-12 06:19:04 +0000

    checking middle for feedback

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,8 +13,4 @@ module.exports = assertArraysEqual;
 
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
