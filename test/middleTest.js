a58a5a49c2881e837f84e5539a653a30f8859084
Jasjot Gill <jasjot97@gmail.com>
2024-03-12 06:22:37 +0000

    fixed issues with the middletest file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,10 @@
 
 const middle = require('../middle');
-const assertEqual = require('../assertEqual');
+const assertArraysEqual = require('../assertArraysEqual');
 
-console.log(middle([1]));             // => []
-console.log(middle([1, 2]));          // => []
-console.log(middle([1, 2, 3]));       // => [2]
-console.log(middle([1, 2, 3, 4, 5]));  // => [3]
-console.log(middle([1, 2, 3, 4]));     // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6]));// => [3, 4]
+assertArraysEqual(middle([1]));             // => []
+assertArraysEqual(middle([1, 2]));          // => []
+assertArraysEqual(middle([1, 2, 3]));       // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]));  // => [3]
+assertArraysEqual(middle([1, 2, 3, 4]));     // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]));// => [3, 4]
